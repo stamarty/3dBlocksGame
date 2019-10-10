2c164eb6a55c6056a52808b1635a2bd90ea2f946
stamarty <7009764+stamarty@users.noreply.github.com>
2019-10-09 20:58:14 -0700

    added objects, movement, collision with objects, fog

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody rb;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
     
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,16 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, 1500 * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce((sidewaysForce * -1) * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
     }
 }
